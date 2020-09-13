cae4066dedcbfc6445e0a1f2330f8eb1d989249e
mayhendra <mayhendra.p10@gmail.com>
2020-09-14 01:18:25 +0700

    change illustration & setting size for each device, fix responsive for tablet device, fix button position on mobile screen

--- src/page/Home.js
+++ src/page/Home.js
@@ -9,18 +9,18 @@ export default function Home() {
     return (
         <div>
             <Navbar searchVisible="false" />
-            <main className="container mx-auto mt-8 md:mt-15 px-6 mb-10">
-                <div className="flex flex-col-reverse items-center md:flex-row md:justify-around">
-                    <div className="flex flex-col md:w-2/5 md:ml-24">
-                        <div className="mt-8">
-                            <h3 className="tracking-wide text-center text-xl font-semibold break-words leading-loose md:leading-relaxed md:text-left md:text-4xl">Tempat lengkap kebutuhan <span className="text-teal-600">Gadged</span> & macam - macam <span className="text-teal-600">Aksesoris Komputer.</span></h3>
+            <main className="container mx-auto mt-8 md:mt-10 lg:mt-15 px-6 mb-10">
+                <div className="flex flex-col-reverse items-center lg:flex-row lg:justify-center">
+                    <div className="flex flex-col lg:w-2/5">
+                        <div className="mt-8 px-4 md:px-16 lg:px-0 lg:mt-0">
+                            <h3 className="tracking-wide text-center text-xl font-semibold break-words leading-loose md:text-4xl md:mt-5 lg:leading-relaxed lg:text-left lg:text-4xl">Tempat lengkap kebutuhan <span className="text-teal-600">Gadged</span> & macam - macam <span className="text-teal-600">Aksesoris Komputer.</span></h3>
                         </div>
-                        <div className="flex justify-center mt-1 md:block md:w-full md:mt-8">
-                            <Link className="px-16 py-3 bg-teal-600 rounded-lg text-white" to="/belanja">Lihat sekarang</Link>
+                        <div className="flex justify-center mt-8 md:mt-10 lg:block lg:w-full lg:mt-12">
+                            <Link className="px-10 py-2 bg-teal-600 rounded-lg text-white md:text-lg md:px-16 md:py-3" to="/belanja">Lihat sekarang</Link>
                         </div>
                     </div>
-                    <div className="flex justify-center mt-8 md:w-2/5 md:justify-center md:items-center">
-                        <img className="w-64 md:w-auto" src={PhoneImg} alt="phone" />
+                    <div className="flex justify-center mt-5 lg:w-2/5 lg:justify-end lg:items-center">
+                        <img className="w-40 md:w-56 lg:w-64 lg:w-auto" src={PhoneImg} alt="phone" />
                     </div>
                 </div>
             </main>
